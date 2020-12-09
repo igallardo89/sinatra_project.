b34e2b3ee806a99f1e7ddde9ccfab6ac729628ca
igallardo89 <igallardo0716@gmail.com>
2020-12-09 14:25:54 -0500

    working

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,12 +6,18 @@ class UsersController < ApplicationController
 
   post "/signup" do
     @user = User.new(:username => params[:username], :email => parmas[:email], :password => parmas[:password])
+    if user.save
+      redirect to '/login'
+    else 
+      flash[message] = "Seems like something wrong try again"
+      redirect to 'signup'
    # needs to create the user and the eventually login them in to acount 
    # give the option of redirecting to main page
+    end
   end
 
   #get "/login" do
-    erb :'/user/login'
+    #erb :'/user/login'
     #this find the user id  then redirect to show the session
     #redirect to sign up if they arent a user
   #end
