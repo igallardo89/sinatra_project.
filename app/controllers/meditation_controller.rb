76fc2f8faf935a648565270c7e2a67a3482078d1
igallardo89 <igallardo0716@gmail.com>
2020-12-10 18:02:08 -0500

    post meditation

--- app/controllers/meditation_controller.rb
+++ app/controllers/meditation_controller.rb
@@ -2,23 +2,29 @@
 class MeditationsController < ApplicationController
 
   get "/meditations" do
-      authenticate
-      @user = current_user
-      @meditations = Meditations.all 
-      erb :'/meditation/index'
-  end
-
-  post "/meditations" do 
+    authenticate
+    @user = current_user
+    @meditations = Meditations.all 
     erb :'/meditation/index'
   end
 
   get "/meditations/new" do 
+    authenticate
+    @meditation = Meditation.new
     erb :'/meditation/new'
   end
 
-  post "/meditations/new" do 
-    erb :'/meditation/new'
-  end
+  post "/meditations" do 
+    @meditation = Meditation.create(date: params[:date], meditation_length: params[:meditation_length], time_of_date: params[:time_of_date])
+      if @meditation.error.any?
+        erb :'/meditation/new'
+      else
+        erb :'/meditation/show'
+      end
+    end
+    
+
+     
 
 
 end
